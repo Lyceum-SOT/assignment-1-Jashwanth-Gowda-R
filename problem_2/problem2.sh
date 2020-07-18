e828b0fe670a753c35d7bb75f3a9f815adb7826a
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2020-07-19 01:28:17 +0530

    curl the website https://wttr.in and outputs to problem_2.txt

--- problem_2/problem2.sh
+++ problem_2/problem2.sh
@@ -2,5 +2,5 @@
 curl -s https://wttr.in > ~/problem_2.txt
 
 grep "Mon 20 Jul " ~/problem_2.txt |
-sed " s/-//g; s/|//g; s/║/;/g; s/│/;/g" |
-awk -F'|' '{print "morning: "$0" ;noon: "$4" ; evening: "$5" ; night: "$6""}'
+  sed " s/-//g; s/|//g; s/║/;/g; s/│/;/g" |
+  awk -F'|' '{print "morning: "$0" ;noon: "$4" ; evening: "$5" ; night: "$6""}'
