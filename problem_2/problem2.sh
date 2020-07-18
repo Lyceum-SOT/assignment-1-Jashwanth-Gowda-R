fd18342d13a448c8b1bcecdbc1b958732920fbf4
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2020-07-19 01:10:37 +0530

    half done

--- problem_2/problem2.sh
+++ problem_2/problem2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -s https://wttr.in > ~/desktop/weather
+curl -s https://wttr.in > ~/problem_2.txt
 
-grep "Mon 20 Jul " ~/desktop/weather |
+grep "Mon 20 Jul " ~/problem_2.txt |
 sed " s/-//g; s/|//g; s/║/;/g; s/│/;/g" |
 awk -F'|' '{print "morning: "$0" ;noon: "$4" ; evening: "$5" ; night: "$6""}'
